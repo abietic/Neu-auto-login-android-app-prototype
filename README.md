e40aacc1331eb456b19c58c4579afd2c924ca74e
abietic <iamyhc@live.cn>
2021-10-25 11:56:53 +0800

    version 0.0.1, A shabby prototype

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Neu-auto-login-android-app-prototype
 An Android app to login neu network automatically
+
+在visual studio 2019中使用xamarin和Android支持编写的东北大学校园网一键登录程序
+
+需要自行在代码中添加自己统一身份认证用的学号和密码（明文，注意个人信息安全），并进行编译安装到手机，在手机连入校园网wifi后，打开app直到页面显示网络已连接的网页
+
+程序执行流程：设置在网页加载完毕后插入自动登录用的js执行代码，打开使用统一身份认证进行网络登录的url，当url对应网页加载完毕，js代码执行，校园网登录完毕
+
+注：需要在manifest xml中申明app使用网络
---
+++ android_neu_auto_login/android_neu_auto_login/MainActivity.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using Android.Webkit;
+
+namespace android_neu_auto_login
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private WebView mWebView;
+        private class CustWebViewClient : WebViewClient
+        {
+            public override void OnPageFinished(WebView w, string url)
+            {
+                base.OnPageFinished(w, url); // 在网页页面加载结束后，在网页中插入我们要使用的js代码
+                var userName = ""; // 用于登录的学号
+                var passWord = ""; // 学号对应的统一身份认证密码（明文，注意个人信息安全）
+                w.LoadUrl("javascript:(function() { " +
+                "document.getElementById('un').value='"+ userName +"';document.getElementById('pd').value='"+ passWord +"';login();})()"); // js代码
+            }
+            
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+            mWebView = FindViewById<WebView>(Resource.Id.webview);
+            mWebView.LoadUrl("https://pass.neu.edu.cn/tpass/login?service=https%3A%2F%2Fipgw.neu.edu.cn%2Fsrun_cas.php%3Fac_id%3D15"); // 通过统一身份认证登录校园网的url
+            //启用JavaScript
+            mWebView.Settings.JavaScriptEnabled = true;
+            mWebView.Settings.UseWideViewPort = true;
+
+            mWebView.Settings.JavaScriptCanOpenWindowsAutomatically = true;
+
+            mWebView.SetWebViewClient(new CustWebViewClient());
+            
+        
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
